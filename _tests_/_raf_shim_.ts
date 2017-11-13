f6bb25668c4ac34aec588452a62fd1aa350b98a7
Jeffrey Priebe <jeffreypriebe@gmail.com>
2017-11-13 12:18:34 -0800

    Minor fixes from inital review
    de-😱

--- _tests_/_raf_shim_.ts
+++ _tests_/_raf_shim_.ts
@@ -1,4 +1,4 @@
-// This needs to be before any imports, thus spearate and specced first in jest setupFiles - https://github.com/facebook/jest/issues/4545#issuecomment-332762365
+// This needs to be before any imports, thus spearate and referenced first in jest setupFiles - https://github.com/facebook/jest/issues/4545#issuecomment-332762365
 const g:any = global;
 
 g.requestAnimationFrame = (callback) => {
